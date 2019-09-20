1cb394d134b67660e04b9f487c3e288c670595ab
Umar Al-Kfaween <omar.ka923@gmail.com>
2019-09-20 15:19:17 -0500

    Use tilemaps and fix movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,36 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MovingObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
+    bool moving = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.position += new Vector3(0, 0.2f, 0);
+        if (!moving) {
 
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position += new Vector3(0, -0.2f, 0);
+          int horizontal = 0;      //Used to store the horizontal move direction.
+          int vertical = 0;        //Used to store the vertical move direction.
 
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(0.2f, 0, 0);
 
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += new Vector3(-0.2f, 0, 0);
 
+          //Check if moving horizontally, if so set vertical to zero.
+          if (horizontal != 0)
+          {
+              vertical = 0;
+          }
+
+          //Check if we have a non-zero value for horizontal or vertical
+          if (horizontal != 0 || vertical != 0)
+          {
+              moving = true;
+              //Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
+              //Pass in horizontal and vertical as parameters to specify the direction to move Player in.
+              Move (horizontal, vertical);
+          }
         }
     }
 }
