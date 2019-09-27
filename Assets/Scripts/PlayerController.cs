74f248f30f6466c0baf9efd62d92f5f3dad86db4
Shohaib Shaffiey <sas87f@gmail.com>
2019-09-27 15:00:02 -0500

    animation link

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     private bool handled = true;
     Rigidbody2D _rb;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+       // anim.Play("upidle");
         if (handled)
         {
 
@@ -24,22 +29,34 @@ public class PlayerController : MonoBehaviour
             {
 
                 transform.position += new Vector3(0, 0.5f, 0);
-
+                anim.Play("moveForward");
             }
+           // else
+           // {
+           //     anim.Play("idleLeft");
+           // }
+           
+            
             if (Input.GetKey(KeyCode.DownArrow))
             {
                 transform.position += new Vector3(0, -0.5f, 0);
+                anim.Play("moveBackward");
 
             }
+          //  else
+         //   {
+         //       anim.Play("idleBackward");
+         //   }
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 transform.position += new Vector3(0.5f, 0, 0);
-
+                anim.Play("moveRight");
             }
             if (Input.GetKey(KeyCode.LeftArrow))
             {
                 transform.position += new Vector3(-0.5f, 0, 0);
-
+                anim.Play("moveLeft");
+                //anim.Play("moveLeft");
             }
         }
         IEnumerator Example()
