48610140d6fc7f00b09b4ec915328135b4eda81c
Shohaib Shaffiey <sas87f@gmail.com>
2019-10-09 19:39:36 -0500

    fixed obstacle distances

--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -7,9 +7,12 @@ public class ObstacleMovement : MonoBehaviour
 {
     private Vector3 _startPosition;
     
-    private float distance;//amp
+    private float distance=3;//amp
     private float speed=1f;//frequency
+    private float frequency = 0.166f;//frequency = 0.5 for 9x16, frequency = 0.1666f for 16x9
     private float screenWidth = Screen.width;
+    private float amplitude = 18;//amplitude = 6 for 9x16, amplitude = 18 for 16x9
+    private float offset = 15;//offset = 5 for 9x16, offset = 15 for 16x9
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +26,7 @@ public class ObstacleMovement : MonoBehaviour
     void Update()
     {
         
-        transform.position = _startPosition + new Vector3((distance*Mathf.Sin(speed*Time.time+Mathf.PI/2*3)),0.0f , 0.0f);
+        transform.position = _startPosition + new Vector3(amplitude*Mathf.Sin(frequency*Time.time+3*Mathf.PI/2)+offset,0.0f , 0.0f);
         //print(distance * Mathf.Sin(speed * Time.time + Mathf.PI / 2));
     }
 }
