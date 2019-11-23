54631745aa3fd1ce69979de45323a3d233557f0f
Amie <a.deng@wustl.edu>
2019-11-23 17:22:47 -0600

    progress

---
+++ Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    public void RestartGame()
+    {
+
+    }
+
+    public void QuitToMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,4 +9,9 @@ public class MainMenu : MonoBehaviour
   {
     SceneManager.LoadScene("Game");
   }
+
+  public void resetHighscore()
+  {
+      GameManager.instance.resetHighscore();
+  }
 }
