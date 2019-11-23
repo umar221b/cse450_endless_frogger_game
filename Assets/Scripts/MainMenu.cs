8113aefec5fc2cc2979e66234a5e500c12ca0813
Umar Al-Kfaween <omar.ka923@gmail.com>
2019-11-23 15:10:29 -0600

    Load Game scene by name

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
   public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+  {
+    SceneManager.LoadScene("Game");
+  }
 }
