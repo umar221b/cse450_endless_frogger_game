b1221c4f7158e9914e47fced9646252e0b65926d
Umar Al-Kfaween <omar.ka923@gmail.com>
2019-11-23 17:28:48 -0600

    Update variable name

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,7 @@ class PlayerController : MonoBehaviour
     Horizontal,
     Vertical
   };
+
   private Orientation gridOrientation = Orientation.Vertical;
   private MonsterManager monsterManager;
   private Vector2 input;
@@ -25,20 +26,18 @@ class PlayerController : MonoBehaviour
   private Vector3 endPosition;
   private float t;
   private float factor = 1f;
+  private bool firstScreenScanned = false;
 
   int counter = 0;
   int counterMax = 0;
 
-  void Start()
-  {
+  void Start() {
     anim = GetComponent<Animator>();
     mainCamera = GameManager.instance.getMainCamera();
     monsterManager = GameManager.instance.getMonsterManager();
-       
-    }
+  }
 
-    public void Update()
-  {
+  public void Update() {
     if(GameManager.instance.gamePaused())
     return;
 
@@ -76,37 +75,15 @@ class PlayerController : MonoBehaviour
       }
     }
   }
-    bool started = true;
+
   public void LateUpdate() {
     Vector3 cameraPosition = mainCamera.transform.position;
     float cameraHeight = mainCamera.GetComponent<Camera>().orthographicSize;
-        //print("transform");
-        //print(transform.position.y);
-        //print(cameraPosition.y);
-        if (started)
-
-        {
-            moveAStarGrid(mainCamera.transform.position);
-            print("something");
-            started = false;
-        }
-            //    
-
-        //print("someting");
-        //    NavGraph[] graphs = AstarPath.active.data.graphs;
-        //    foreach (NavGraph graph in graphs)
-        //    {
-        //        GridGraph gg = (GridGraph)graph;
-                
-        //        cameraPosition.z = 0;
-        //        gg.center = cameraPosition;
-        //        gg.Scan();
-        //    }
-     //   }
-
-      if (transform.position.y > cameraPosition.y + cameraHeight) {
-      //Destroy(GameObject.FindGameObjectWithTag("Keese"));
-      //   GameManager.instance.Astar.GetComponent<AstarPath>().graphs.GetValue.GetComponentInParent
+    if (!firstScreenScanned) {
+      moveAStarGrid(mainCamera.transform.position);
+      firstScreenScanned = true;
+    }
+    if (transform.position.y > cameraPosition.y + cameraHeight) {
       monsterManager.MoveSpawnPoints(1);
       mainCamera.transform.position = cameraPosition + new Vector3(0, 16f, 0);
       int newActiveWorldPart = GameManager.instance.getActiveWorldPart();
@@ -116,14 +93,10 @@ class PlayerController : MonoBehaviour
       }
       moveAStarGrid(mainCamera.transform.position);
     }
-
     else if (transform.position.y < cameraPosition.y - cameraHeight) {
-
-      //Destroy(GameObject.FindGameObjectWithTag("Keese"));
       monsterManager.MoveSpawnPoints(-1);
       mainCamera.transform.position = cameraPosition - new Vector3(0, 16f, 0);
       moveAStarGrid(mainCamera.transform.position);
-
     }
   }
 
