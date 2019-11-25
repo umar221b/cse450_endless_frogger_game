b05978e47285708d1eaf5f29cb46adabb05f1ff1
Umar Al-Kfaween <omar.ka923@gmail.com>
2019-11-25 15:18:43 -0600

    Add score and highscore to death menu

---
+++ Assets/Scripts/DataLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataLoader : MonoBehaviour {
+  public Text scoreValueText;
+  public Text highscoreValueText;
+
+  void Awake() {
+    int score = PlayerPrefs.GetInt("score");
+    int highscore = PlayerPrefs.GetInt("highscore");
+    scoreValueText.text = score.ToString();
+    highscoreValueText.text = highscore.ToString();
+  }
+}
