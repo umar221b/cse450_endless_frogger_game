6ec8501ab61d520f292fdf6aff9c836165d3961d
Umar Al-Kfaween <omar.ka923@gmail.com>
2019-12-03 19:18:59 -0600

    Patch up changing volume on scene restart

--- Assets/Scripts/SetVolume.cs
+++ Assets/Scripts/SetVolume.cs
@@ -1,15 +1,29 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class SetVolume : MonoBehaviour
 {
-    public AudioMixer mixer;
+  public AudioMixer mixer;
+  private Slider slider;
 
+  void Awake() {
+    float storedSliderValue;
 
-    public void SetLevel(float sliderValue)
-    {
-        mixer.SetFloat("MusicVolume", Mathf.Log10(Mathf.Max(sliderValue, 0.0001f))*20f);
-    }
+    if (PlayerPrefs.HasKey("music_slider_value"))
+      storedSliderValue = PlayerPrefs.GetFloat("music_slider_value");
+    else
+      storedSliderValue = 1;
+
+    slider = GetComponent<Slider>();
+    slider.value = storedSliderValue;
+  }
+
+  public void SetLevel(float sliderValue)
+  {
+    mixer.SetFloat("MusicVolume", Mathf.Log10(Mathf.Max(sliderValue, 0.0001f))*20f);
+    PlayerPrefs.SetFloat("music_slider_value", sliderValue);
+  }
 }
